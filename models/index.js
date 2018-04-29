24c32dbb33b2ba0be886c21dfa31624669f8ab61
Quentin McKay <quentinmckay1@gmail.com>
2018-04-29 23:33:08 +1000

    changed for heroku deploy

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.set('debug', true)  // lets us see what's happening
 
-// const dbURL = process.env.DATABASEURL || 'mongodb://localhost/todo-api'
-const dbURL = process.env.DATABASEURL || 'mongodb://quentinmckay:bubbles1357@ds245548.mlab.com:45548/adv-wbdev-express-todolist'
+const dbURL = process.env.DATABASEURL || 'mongodb://localhost/todo-api'
+// const dbURL = process.env.DATABASEURL || 'mongodb://quentinmckay:bubbles1357@ds245548.mlab.com:45548/adv-wbdev-express-todolist'
 mongoose.connect(dbURL)  // mongo will create 'todo-api' database if it doesn't exist
 
 mongoose.Promise = Promise  // allows us to use Promises rather than callbacks (set to Promise rather than external Promise library)
